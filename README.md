21a4c065229e333edae84147a402ba31f2845c78
Tymek Slomski <tymekslomski@gmail.com>
2023-05-19 12:13:28 +0200

    generating invalid fields
    We can add as many wrong fields as we want.
    The function checks whether they have already been taken, which means that they are never repeated.

---
+++ README.md
@@ -0,0 +1,2 @@
+Educational game
+It consists of finding the bug in the vegetables, and answering the question correctly. If the player answers the question correctly, the worm is eliminated. If we answer the question incorrectly, the worm changes place.
--- boardGenerator.js
+++ boardGenerator.js
@@ -1,26 +1,31 @@
 const container = document.querySelector(".container");
 
-let array = [];
-
-function makeRows(rows, cols) {
+const makeRows = (rows, cols) => {
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
   for (i = 0; i < rows * cols; i++) {
     let cell = document.createElement("div");
-    array.push(cell);
     cell.innerText = i + 1;
     container.appendChild(cell).className = "grid-item";
   }
-}
+};
 
 makeRows(10, 10);
 
-const special = () => {
+const special = (l) => {
   const divAll = document.querySelectorAll(".grid-item");
-  console.log(divAll);
-  const randomIndex = Math.floor(Math.random() * divAll.length);
-  const randomDiv = divAll[randomIndex];
-  console.log(randomDiv);
-  randomDiv.classList.add("gridSpecial");
+  let indexArr = [];
+  for (i = 0; i < l; i++) {
+    const randomIndex = Math.floor(Math.random() * divAll.length);
+    if (indexArr.includes(randomIndex)) {
+      i--;
+      continue;
+    } else {
+      const randomDiv = divAll[randomIndex];
+      randomDiv.classList.add("gridSpecial");
+      indexArr.push(randomIndex);
+    }
+  }
 };
-special();
+
+special(10);
